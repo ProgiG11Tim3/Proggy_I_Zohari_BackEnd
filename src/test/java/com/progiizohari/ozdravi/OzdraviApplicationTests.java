cba70e5edb3ad4f126661bacf6551c7bc55a3fe3
Dino <84743074+LeapersEdge@users.noreply.github.com>
2023-11-01 15:35:47 +0100

    init commit
    setup maven spring boot with following dependencies:
    - spring web
    - spring data JPA
    - H2 database
    - validation
    - postgreSQL driver

---
+++ src/test/java/com/progiizohari/ozdravi/OzdraviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progiizohari.ozdravi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OzdraviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
