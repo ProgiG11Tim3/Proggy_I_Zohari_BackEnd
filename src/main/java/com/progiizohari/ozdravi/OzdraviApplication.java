1f45ab6785b35cb0122458f150a20b8db58d0dff
lokiMatic <lovromatic07@gmail.com>
2023-11-10 17:14:02 +0100

    Ispravak Lukinog commita treba pripaziti i spojiti repository i oib ne smije sadrzavati znakove u njemu

--- src/main/java/com/progiizohari/ozdravi/OzdraviApplication.java
+++ src/main/java/com/progiizohari/ozdravi/OzdraviApplication.java
@@ -4,6 +4,7 @@ import com.progiizohari.ozdravi.domain.Parent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.progiizohari.ozdravi.repositories.ParentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +18,8 @@ import java.time.LocalDate;
 @RestController
 public class OzdraviApplication {
     private static final Logger log = LoggerFactory.getLogger(OzdraviApplication.class);
+
+    @Autowired
     private ParentRepository repository;
 
     public static void main(String[] args) {
@@ -26,7 +29,7 @@ public class OzdraviApplication {
     @Bean
     public CommandLineRunner demo() {
         return (args) -> {
-            Parent testParent = new Parent(1, "oib12345678", "imeRoditelja",
+            Parent testParent = new Parent(1, "12345678910", "imeRoditelja",
                     "prezimeRoditelja", LocalDate.of(2023, 11, 7),
                     "userNameRoditelja", "passwordRoditelja", "brojMobitelaRoditelja",
                     "emailRoditelja@gmail.com", 10000, "Zagreb", "emailPoslodavca@gmail.com",
