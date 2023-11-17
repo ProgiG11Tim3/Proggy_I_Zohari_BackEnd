aba9f39c270dc9b3bde1e54f513a351f2d9b3a79
Luka Bračun <lukabracun7127@gmail.com>
2023-11-17 15:23:03 +0100

    Added remaining entities

---
+++ src/main/java/com/progiizohari/ozdravi/domain/SickNote.java
@@ -0,0 +1,60 @@
+package com.progiizohari.ozdravi.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class SickNote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int excuseNoteId;
+
+    @ManyToOne
+    @JoinColumn(name = "child_OIB")
+    private Child child;
+
+    @NotNull
+    private String noteData;
+
+    public SickNote() {
+    }
+
+    public SickNote(int excuseNoteId, Child child, String noteData) {
+        this.excuseNoteId = excuseNoteId;
+        this.child = child;
+        this.noteData = noteData;
+    }
+
+    public int getExcuseNoteId() {
+        return excuseNoteId;
+    }
+
+    public void setExcuseNoteId(int excuseNoteId) {
+        this.excuseNoteId = excuseNoteId;
+    }
+
+    public Child getChild() {
+        return child;
+    }
+
+    public void setChild(Child child) {
+        this.child = child;
+    }
+
+    public String getNoteData() {
+        return noteData;
+    }
+
+    public void setNoteData(String noteData) {
+        this.noteData = noteData;
+    }
+
+    @Override
+    public String toString() {
+        return "SickNote{" +
+                "excuseNoteId=" + excuseNoteId +
+                ", child=" + child +
+                ", noteData='" + noteData + '\'' +
+                '}';
+    }
+}
