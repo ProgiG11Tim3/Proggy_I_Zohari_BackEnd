8938e978e551d79578ab4212529d1f4748b36f5e
Dino <84743074+LeapersEdge@users.noreply.github.com>
2024-01-17 11:33:16 +0100

    nvm

--- src/main/java/com/progiizohari/ozdravi/controllers/ChildController.java
+++ src/main/java/com/progiizohari/ozdravi/controllers/ChildController.java
@@ -10,21 +10,5 @@ public class ChildController {
 
     @Autowired
     private ChildService service;
-
-    @GetMapping("/getChildByOIB{oib}")
-    public String getChildByOIB(String oib) {
-        if (oib == null) {
-            return "OIB is null";
-        }
-
-        if (oib.length() != 11) {
-            return "OIB is not 11 characters long";
-        }
-
-        if (!oib.matches("[0-9]+")) {
-            return "OIB contains non-numeric characters";
-        }
-
-        return service.getChildByOIB(oib);
-    }
+    
 }
