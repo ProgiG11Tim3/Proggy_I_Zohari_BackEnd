d46ba3279c91d45d1621572bbd6f88e34e491d5c
lovrom <lovromatic07@gmail.com>
2024-01-19 19:16:47 +0100

    Update upload FileDB

--- src/main/java/com/progiizohari/ozdravi/repositories/FileRepository.java
+++ src/main/java/com/progiizohari/ozdravi/repositories/FileRepository.java
@@ -4,4 +4,5 @@ import com.progiizohari.ozdravi.domain.FileDB;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FileRepository  extends JpaRepository<FileDB, String> {
+
 }
--- src/main/java/com/progiizohari/ozdravi/repositories/MedicalReportRepository.java
+++ src/main/java/com/progiizohari/ozdravi/repositories/MedicalReportRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MedicalReportRepository extends JpaRepository<MedicalReport, Integer> {
     MedicalReport findByReportId(int reportId);
+
+    MedicalReport findMedicalReportByReportId(int reportId);
 }
