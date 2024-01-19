5d969e73c36b80a250e0785eb62cfefe3187693a
Dino <84743074+LeapersEdge@users.noreply.github.com>
2024-01-19 23:30:13 +0100

    aaaa

---
+++ src/main/java/com/progiizohari/ozdravi/bootstrap/ShellCommandLineRunner.java
@@ -0,0 +1,23 @@
+package com.progiizohari.ozdravi.bootstrap;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.shell.Shell;
+import org.springframework.stereotype.Component;
+/*
+@Component
+@Order(2)
+public class ShellCommandLineRunner implements CommandLineRunner{
+    @Autowired
+    private Shell shell;
+
+    @Autowired
+    private DataLoader dataLoader;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Shell has been initialized. You can add additional initialization logic here.");
+    }
+}
+*/
